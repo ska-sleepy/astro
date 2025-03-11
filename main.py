95df47da6b0b38cd9843300343b1ea4cc025a782
ska_sleepy <noureldinabouzeid@gmail.com>
2025-03-11 15:04:22 +0000

    added stuff

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
 
@@ -12,7 +15,8 @@ def main():
             
         pygame.Surface.fill(screen, (00,00,00))
         pygame.display.flip()
-
+        framerate = clock.tick(60)
+        dt = framerate / 1000
 
 if __name__ == "__main__":
     main()
